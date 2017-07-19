775cc07b90fcf5f69c026d8fb4ee3c5ab3face7a
sasha <skitale85@gmail.com>
2017-07-19 05:42:53 +0300

    version 2

--- main.rb
+++ main.rb
@@ -13,22 +13,22 @@ require_relative 'lib/product'
 require_relative 'lib/book'
 require_relative 'lib/film'
 require_relative 'lib/disk'
-require_relative 'lib/kass'
+require_relative 'lib/shop'
 require_relative 'lib/product_collection'
 
 path = "data"
 
-kass = Kass.new(path)
+shop = Shop.new(path)
 loop do
   system "cls"
   puts "\n--- Что бы вы хотели купить? Мы можем вам предложить:\n\n"
-  kass.show_list_of_goods
+  shop.show_list_of_goods
   puts "0. Выход\n\n"
-  kass.show_cart
-  puts "\nСумма: #{kass.total}"
+  shop.show_cart
+  puts "\nСумма: #{shop.total}"
   user_input = STDIN.gets.chomp
   break if user_input == "0"
   item_number = user_input.to_i.abs - 1
-  kass.add_to_cart(item_number)
+  shop.add_to_cart(item_number)
 end
-puts "\nС вас #{kass.total} руб."
+puts "\nС вас #{shop.total} руб."
